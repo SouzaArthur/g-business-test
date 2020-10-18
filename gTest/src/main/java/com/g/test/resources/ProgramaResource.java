c28fc0e46435a88514f5506624e8f11cf987d15f
Arthur Souza <arthurr.souzaa98@gmail.com>
2020-10-18 15:42:30 -0300

    feature: Implementando método put
    Implementando método put para Programa

--- gTest/src/main/java/com/g/test/resources/ProgramaResource.java
+++ gTest/src/main/java/com/g/test/resources/ProgramaResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -70,6 +71,18 @@ public class ProgramaResource {
 		return ResponseEntity.ok().body(obj);
 	}
 	
+	@PutMapping(value="/{idString}")
+	public ResponseEntity<Void> updateProgram(@Valid @RequestBody ProgramaDTO objDto, @PathVariable String idString){
+		Programa obj = service.fromDto(objDto);
+		
+		//Setando a id do objeto recebido no body, para que seja o mesmo recebido na URL
+		obj.setId(idString);
+		
+		service.update(obj);
+		
+		return ResponseEntity.noContent().build();
+		
+	}
 	
 	
 }
